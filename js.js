42493a67bcd20a37e657b8806f0615c2a85b031c
wilsonbirch <wilsonbirch@gmail.com>
2021-01-24 01:47:17 -0500

    added to git repo

---
+++ js.js
@@ -0,0 +1,24 @@
+$('#car1').carousel({
+    interval: 1500,
+    wrap: false
+  })
+
+$('#car2').carousel({
+    interval: 3000,
+    wrap: false
+})
+
+$('#car3').carousel({
+    interval: 4500,
+    wrap: false
+})
+
+$('#car4').carousel({
+    interval: 6000,
+    wrap: false
+})
+
+$('#car5').carousel({
+    interval: 7500,
+    wrap: false
+})
